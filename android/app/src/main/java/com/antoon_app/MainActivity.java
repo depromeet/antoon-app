1e12f72e0f8e9e0454bc588a701ecfaa1336615d
Lee Byonghun <dlqud19@gmail.com>
2022-08-10 03:32:02 +0900

    안드로이드 배포 설정 (#1)

--- android/app/src/main/java/com/antoon_app/MainActivity.java
+++ android/app/src/main/java/com/antoon_app/MainActivity.java
@@ -1,13 +1,23 @@
 package com.antoon_app;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 
+import org.devio.rn.splashscreen.SplashScreen;
+
 public class MainActivity extends ReactActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
+    super.onCreate(savedInstanceState);
+  }
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * Returns the name of the main component registered from JavaScript. This is
+   * used to schedule
    * rendering of the component.
    */
   @Override
@@ -16,7 +26,8 @@ public class MainActivity extends ReactActivity {
   }
 
   /**
-   * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
+   * Returns the instance of the {@link ReactActivityDelegate}. There the RootView
+   * is created and
    * you can specify the rendered you wish to use (Fabric or the older renderer).
    */
   @Override
